c2c17f754b59bd81196da7cc231fd491bb12b48c
Alex Gao <143097174+Ga0-Alex@users.noreply.github.com>
2024-04-17 14:29:37 -0500

    Update ItemToPurchase.cpp

--- ItemToPurchase.cpp
+++ ItemToPurchase.cpp
@@ -1,5 +1,27 @@
 #include <iostream>
 using namespace std;
 
+
 #include "ItemToPurchase.h"
+
+ItemToPurchase::ItemToPurchase() : itemName("None"), itemPrice(0), itemQuantity(0) {}
+
+void ItemToPurchase::SetName(const string &name) {
+  itemName = name;
+}
+void ItemToPurchase::SetPrice(int price){
+  itemPrice = price;
+}
+void ItemToPurchase::SetQuantity(int quantity){
+itemQuantity = quantity;
+}
+string ItemToPurchase::GetName(){
+  return itemName;
+}
+string ItemToPurchase::GetPrice(){
+  return itemPrice;
+}
+string ItemToPurchase::GetQuantity(){
+  return itemQuantity;
+}
 
